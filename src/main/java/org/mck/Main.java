aa3e948259241bf2b63cc8465a09b977d68e1750
MATHC <92028037+Math23Kskb@users.noreply.github.com>
2025-02-23 19:22:21 -0300

    feat(ui): update Main.java to launch SudokuFX application

--- src/main/java/org/mck/Main.java
+++ src/main/java/org/mck/Main.java
@@ -1,12 +1,32 @@
 package org.mck;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Console Sudoku!");
-        new SudokuGame().play();
-        System.out.println("Thanks for playing!");
+public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/MainView.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/style.css")).toExternalForm());
+
+        primaryStage.setTitle("Sudoku Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+    public static void main(String[] args)  {
+        launch(args);
     }
 }
