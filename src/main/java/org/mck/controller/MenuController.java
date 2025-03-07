14cbef4061950779ae485596ff374a2907c7c379
MATHC <92028037+Math23Kskb@users.noreply.github.com>
2025-03-06 21:14:34 -0300

    refactor(MenuController): Rewrite for game setup and programmatic controller loading

--- src/main/java/org/mck/controller/MenuController.java
+++ src/main/java/org/mck/controller/MenuController.java
@@ -4,7 +4,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import org.mck.generator.BoardGenerator;
+import org.mck.generator.SudokuGenerator;
+import org.mck.generator.SudokuInitializer;
+import org.mck.generator.SudokuRemover;
+import org.mck.model.Board;
+import org.mck.model.SudokuBoard;
+import org.mck.solver.SudokuSolver;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -17,9 +25,27 @@ public class MenuController {
     @FXML
     public void startNewGame() throws IOException {
 
+        Board board = new SudokuBoard();
+
+        BoardGenerator boardGenerator = new SudokuGenerator(
+                new SudokuInitializer(
+                        new SudokuSolver()), new SudokuRemover(50)
+        );
+
+        GameController gameController = new GameController(board, boardGenerator);
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/GameView.fxml"));
+
+        loader.setController(gameController);
+
+        GridPane boardGridFromFXML;
+
         Scene gameScene = new Scene(loader.load());
         gameScene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/style.css")).toExternalForm());
+
+        boardGridFromFXML = (GridPane) gameScene.getRoot();
+        SudokuBoardView boardView = new SudokuBoardView(boardGridFromFXML, board, gameController);
+        gameController.setBoardView(boardView);
 
         Stage stage = (Stage) startGameButton.getScene().getWindow();
         stage.setScene(gameScene);
