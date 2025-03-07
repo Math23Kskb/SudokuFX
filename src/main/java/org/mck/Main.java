30f5bff93f5acf1f6ba4e11c9cf83e0b8930eb4d
MATHC <92028037+Math23Kskb@users.noreply.github.com>
2025-03-06 21:17:19 -0300

    feat(ui): Update Main.java to launch JavaFX menu application

--- src/main/java/org/mck/Main.java
+++ src/main/java/org/mck/Main.java
@@ -3,8 +3,8 @@ package org.mck;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 
 import java.util.Objects;
 
@@ -14,14 +14,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/MainView.fxml"));
-        Scene scene = new Scene(loader.load());
 
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/style.css")).toExternalForm());
+            FXMLLoader menuLoader = new FXMLLoader(Main.class.getResource("/fxml/MainView.fxml"));
+            VBox menuRoot = menuLoader.load();
+            Scene menuScene = new Scene(menuRoot);
+            menuScene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/style.css")).toExternalForm());
 
-        primaryStage.setTitle("Sudoku Game");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            primaryStage.setTitle("Sudoku Game");
+            primaryStage.setScene(menuScene);
+            primaryStage.show();
 
     }
 
