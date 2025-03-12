688b057b361fa8956edd2a7c1768d01dbf695287
MATHC <92028037+Math23Kskb@users.noreply.github.com>
2025-03-12 20:05:57 -0300

    feat(Board): extend Board interface with Sudoku rule validation

--- src/main/java/org/mck/model/Board.java
+++ src/main/java/org/mck/model/Board.java
@@ -5,7 +5,12 @@ public interface Board {
     void setValue(int row, int col, int num);
     boolean isValidMove(int row, int col, int num);
     boolean isCellEmpty(int row, int col);
-    void resetBoard();
-    int getSize(); // Added to get board size
+    int getSize();
     int getSubgridSize();
+
+    boolean isNumberInRow(int row, int num);
+    boolean isNumberInColumn(int col, int num);
+    boolean isNumberInBox(int row, int col, int num);
+
+    boolean isBoardComplete();
 }
