8357e29705dd5407c6a996947cfcc9c6d362bc81
MATHC <92028037+Math23Kskb@users.noreply.github.com>
2025-03-12 20:10:13 -0300

    feat(GameController): implement game over and reset functionalities, refactor validation

--- src/main/java/org/mck/controller/GameController.java
+++ src/main/java/org/mck/controller/GameController.java
@@ -2,11 +2,17 @@ package org.mck.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import org.mck.generator.BoardGenerator;
 import org.mck.model.Board;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class GameController {
     private final Board board;
@@ -32,54 +38,55 @@ public class GameController {
         boardView.initializeBoardDisplay();
     }
 
-    @FXML
-    public void handleCellInput(ActionEvent event) {
-        TextField cellField = (TextField) event.getSource();
-        Integer row = GridPane.getRowIndex(cellField);
-        Integer col = GridPane.getColumnIndex(cellField);
-
-        if (row == null || col == null) return;
-
-        String text = cellField.getText();
-
-        if (text != null && !text.isEmpty()) {
-
-            if (!text.matches("[1-9]")) {
-                showInvalidMoveAlert("Please enter digits from 1-9 only.");
-                cellField.clear();
-                return;
-            }
-            int userValue = Integer.parseInt(text);
-
-            if (board.isValidMove(row, col, userValue)) {
-                board.setValue(row, col, userValue);
-            } else {
-                showInvalidMoveAlert("Invalid move! This number violates Sudoku rules.");
-                cellField.clear();
-            }
-
-            boardView.updateBoardDisplay();
-        }
+    public boolean isValidSudokuMove(int row, int col, int num) {
+        return board.isValidMove(row, col, num);
     }
 
-    private void showInvalidMoveAlert(String message) {
+    public void showInvalidMoveAlert(int num, int row, int col) {
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Invalid Move");
-        alert.setHeaderText("The move is not valid!");
-        alert.setContentText(message);
+        alert.setHeaderText("Sudoku Rule Violation!");
+        StringBuilder message = new StringBuilder(String.format("Number %d cannot be placed here because it violates Sudoku Rules\n", num));
+
+        if (board.isNumberInRow(row, num)) {
+            message.append("- It's already in row ").append(row + 1).append("\n");
+        }
+        if (board.isNumberInColumn(col, num)) {
+            message.append("- It's already in column ").append(col + 1).append("\n");
+        }
+        if (board.isNumberInBox(row, col, num)) {
+            message.append("- It's already in the 3x3 block containing this cell.");
+        }
+
+        alert.setContentText(message.toString());
         alert.showAndWait();
     }
 
-/*
-    @FXML
-    public void handleReset() {
-        board.resetBoard();
-        initializeGame();
+    public void GameOver(ActionEvent actionEvent) throws IOException {
+        if (board.isBoardComplete()) {
+            showGameOverWindow(actionEvent);
+        } else {
+            System.out.println("Board is NOT complete yet.");
+        }
     }
 
-    @FXML
-    public void handleExit() {
-        Stage stage = (Stage) boardGrid.getScene().getWindow();
-        stage.close();
-    }*/
+    private void showGameOverWindow(ActionEvent event) throws IOException {
+        FXMLLoader gameOverLoader = new FXMLLoader(getClass().getResource("/fxml/GameOverView.fxml"));
+        Stage gameOverStage = new Stage();
+        Scene gameOverScene = new Scene(gameOverLoader.load());
+        gameOverScene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/style.css")).toExternalForm());
+
+        GameOverController gameOverController = gameOverLoader.getController();
+        gameOverController.setGameController(this); // Pass GameController instance
+
+        gameOverStage.setTitle("Congratulations!");
+        gameOverStage.setScene(gameOverScene);
+        gameOverStage.initOwner(((Node) event.getSource()).getScene().getWindow());
+        gameOverStage.show();
+    }
+
+    public void resetGame() {
+        boardGenerator.generateBoard(board);
+        boardView.initializeBoardDisplay();
+    }
 }
